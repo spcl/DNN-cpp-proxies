2a9a7ad3486c4e6e2a4b6949fc9728d92d18ef8a
Shigang Li <shigang.li@inf.ethz.ch>
2022-03-14 19:31:46 +0100

    res152 scal

--- comm-and-compute/resnet152_scalability.cpp
+++ comm-and-compute/resnet152_scalability.cpp
@@ -22,14 +22,15 @@
 #define NUM_B 10
 int allreduce_sizes[NUM_B] = {6511592, 6567936, 5905920, 6113280, 6176256, 6112768, 6176256, 6112768, 5321216, 5194816};
 
-// batchsize = 128
-// Suggest world_size <= 256, which is corresponding to a global batch_size <= 32 K
+// Global batch_size <= 32 K
 // A100 GPU
-// runtime in us (10E-6) for each iteration 
-int fwd_rt_whole_model = 119000;
-int bwd_rt_per_B = 23800;
+// runtime in us (10E-6) for each iteration
+// corresponding to local batch size = {128, 64, 32, 16}
+int local_batch_size_arr[4] = {128, 64, 32, 16};
+int fwd_rt_whole_model_arr[4] = {119000, 63000, 36000, 27667};
+int bwd_rt_per_B_arr[4] = {23800, 12600, 7200, 5533};
 
-int run_data_parallel(float** grad_ptrs, float** sum_grad_ptrs){
+int run_data_parallel(float** grad_ptrs, float** sum_grad_ptrs, int fwd_rt_whole_model, int bwd_rt_per_B){
     
     //forward
     usleep(fwd_rt_whole_model); //compute
@@ -68,17 +69,45 @@ int main(int argc, char *argv[]){
         sum_grad_ptrs[i] = (float *)calloc(allreduce_sizes[i], sizeof(float));
     }
 
+    int fwd_rt_whole_model, bwd_rt_per_B, local_batch_size;
+
     MPI_Barrier(MPI_COMM_WORLD);
+    switch(world_size){
+        case 256:
+	    fwd_rt_whole_model = fwd_rt_whole_model_arr[0]; 
+            bwd_rt_per_B = bwd_rt_per_B_arr[0];
+	    local_batch_size = local_batch_size_arr[0];
+            break;
+        case 512:
+	    fwd_rt_whole_model = fwd_rt_whole_model_arr[1]; 
+            bwd_rt_per_B = bwd_rt_per_B_arr[1];
+	    local_batch_size = local_batch_size_arr[1];
+            break;
+        case 1024:
+	    fwd_rt_whole_model = fwd_rt_whole_model_arr[2]; 
+            bwd_rt_per_B = bwd_rt_per_B_arr[2];
+	    local_batch_size = local_batch_size_arr[2];
+            break;
+        case 2048:
+	    fwd_rt_whole_model = fwd_rt_whole_model_arr[3]; 
+            bwd_rt_per_B = bwd_rt_per_B_arr[3];
+	    local_batch_size = local_batch_size_arr[3];
+            break;
+        default:
+            printf("Unsupported MPI_Comm_World size!\n");
+	    return 0;
+    }
+
 
     //warmup
     for(int wmp = 0; wmp < WARM_UP; wmp++){
-        run_data_parallel(grad_ptrs, sum_grad_ptrs);
+        run_data_parallel(grad_ptrs, sum_grad_ptrs, fwd_rt_whole_model, bwd_rt_per_B);
     }
 
     double begin, elapse;
     begin = MPI_Wtime();
     for(int iter = 0; iter < RUNS; iter++){
-        run_data_parallel(grad_ptrs, sum_grad_ptrs);
+        run_data_parallel(grad_ptrs, sum_grad_ptrs, fwd_rt_whole_model, bwd_rt_per_B);
     }
     elapse = (MPI_Wtime()-begin)/RUNS;
 
@@ -88,7 +117,7 @@ int main(int argc, char *argv[]){
     }
 
     if(rank == 0){
-        printf("Rank = %d, world_size = %d, data_shards = %d, total_params = %d, global_batch_size = %d. \n", rank, world_size, world_size, total_params, 128*world_size);
+        printf("Rank = %d, world_size = %d, data_shards = %d, total_params = %d, global_batch_size = %d. \n", rank, world_size, world_size, total_params, local_batch_size*world_size);
         printf("ResNet-152 with pure data parallelism runtime for each iteration = %f s. \n", elapse);
     }
 
