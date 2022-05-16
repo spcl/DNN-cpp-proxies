1a255a484d18352171c1cb93de17be5c00384006
Shigang Li <shigang.li@inf.ethz.ch>
2022-05-16 16:18:34 +0200

    dlrm

--- comm-and-compute/dlrm.cpp
+++ comm-and-compute/dlrm.cpp
@@ -1,7 +1,13 @@
 /*********************************************************************
  *
  * Description: DLRM
- *              python -m torch.distributed.launch --nproc_per_node=4 dlrm_s_pytorch.py --arch-embedding-size="80000-80000-80000-80000" --arch-sparse-feature-size=128 --arch-mlp-bot="128-128-128-128" --arch-mlp-top="512-512-512-256-1" --max-ind-range=40000000 --data-generation=random --loss-function=bce --round-targets=True --learning-rate=1.0 --mini-batch-size=2048 --print-freq=2 --print-time --test-freq=16 --test-mini-batch-size=1024 --memory-map --use-gpu --num-batches=32 --dist-backend=nccl  
+ *  python -m torch.distributed.launch --nproc_per_node=4 dlrm_s_pytorch.py 
+ *  --arch-embedding-size="80000-80000-80000-80000" --arch-sparse-feature-size=128 
+ *  --arch-mlp-bot="128-128-128-128" --arch-mlp-top="512-512-512-256-1" 
+ *  --max-ind-range=40000000 --data-generation=random --loss-function=bce 
+ *  --round-targets=True --learning-rate=1.0 --mini-batch-size=2048 --print-freq=2 
+ *  --print-time --test-freq=16 --test-mini-batch-size=1024 
+ *  --memory-map --use-gpu --num-batches=32 --dist-backend=nccl  
  *
  *********************************************************************/
 
